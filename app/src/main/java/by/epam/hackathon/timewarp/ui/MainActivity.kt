628abf37d35b1ca2212398e441f20d3431707b8f
Yahor Fralou <yahor.fralou@datalex.com>
2018-02-02 18:41:49 +0300

    Some assets and animations added

--- app/src/main/java/by/epam/hackathon/timewarp/ui/MainActivity.kt
+++ app/src/main/java/by/epam/hackathon/timewarp/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package by.epam.hackathon.timewarp.ui
 
+import android.animation.ObjectAnimator
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.support.design.widget.Snackbar
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
@@ -16,6 +19,12 @@ class MainActivity : AppCompatActivity() {
 
     private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
+    companion object {
+        val TAB_INDEX_LIST = 0
+        val TAB_INDEX_TIME_CONTROL = 1
+        val TAB_INDEX_REPORT = 2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,6 +36,8 @@ class MainActivity : AppCompatActivity() {
 
         container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+
+        tabs.addOnTabSelectedListener(TabSelectionListener(this))
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -56,19 +67,58 @@ class MainActivity : AppCompatActivity() {
         override fun getItem(position: Int): Fragment {
 
             return when (position) {
-                1 -> {
+                TAB_INDEX_TIME_CONTROL -> {
                     TimeControlFragment.newInstance()
-                } else -> {
+                }
+                else -> {
                     PlaceholderFragment.newInstance(position + 1)
                 }
             }
         }
 
         override fun getCount(): Int {
-            return 3
+            return tabs.tabCount
         }
     }
 
+    private inner class TabSelectionListener(val ctx: Context) : TabLayout.OnTabSelectedListener {
+
+        override fun onTabSelected(tab: TabLayout.Tab?) {
+            fab.rotation = 0f
+            when (tab?.position) {
+                TAB_INDEX_TIME_CONTROL -> {
+                    fab.hide()
+                }
+                TAB_INDEX_LIST -> {
+                    if (fab.visibility == View.VISIBLE) {
+                        rotateButton(0f, -90f, R.drawable.ic_add_24)
+                    } else {
+                        fab.setImageResource(R.drawable.ic_add_24)
+                        fab.show()
+                    }
+                }
+                TAB_INDEX_REPORT -> {
+                    if (fab.visibility == View.VISIBLE) {
+                        rotateButton(-90f, 0f, R.drawable.ic_cloud_upload_24)
+                    } else {
+                        fab.setImageResource(R.drawable.ic_cloud_upload_24)
+                        fab.show()
+                    }
+                }
+            }
+        }
+
+        private fun rotateButton(angleFrom: Float, angleTo: Float, imgNewRes: Int) {
+            ObjectAnimator.ofFloat(fab, "rotation", angleFrom, angleTo).setDuration(200).start();
+            Handler().postDelayed({ fab.setImageResource(imgNewRes) }, 100);
+        }
+
+        override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {}
+    }
+
+    @Deprecated("To be removed")
     class PlaceholderFragment : Fragment() {
 
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
